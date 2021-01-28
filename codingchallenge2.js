f4f998e13f81757818f30209be383587b62fd39b
DivyaVinoth <75644696+DivyaVinoth@users.noreply.github.com>
2021-01-28 07:09:18 +0530

    Add Fundamental Part 1 Files

---
+++ codingchallenge2.js
@@ -0,0 +1,6 @@
+
+if( markBMI > johnBMI ) {
+  console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`);
+} else {
+  console.log(`John's BMI ${markBMI} is higher than Mark's ${johnBMI}!`);
+}
