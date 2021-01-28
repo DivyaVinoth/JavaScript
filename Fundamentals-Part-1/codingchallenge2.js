899209d331b6f226a11d8f83349892bffdc16951
DivyaVinoth <75644696+DivyaVinoth@users.noreply.github.com>
2021-01-28 07:07:22 +0530

    Delete Fundamentals-Part-1 directory

--- Fundamentals-Part-1/codingchallenge2.js
+++
@@ -1,6 +0,0 @@
-
-if( markBMI > johnBMI ) {
-  console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`);
-} else {
-  console.log(`John's BMI ${markBMI} is higher than Mark's ${johnBMI}!`);
-}
--- Fundamentals-Part-1/codingchallenge4.js
+++
@@ -1,4 +0,0 @@
-const billValue = 40;
-const tip = (billValue >= 50 && billValue <= 300) ? billValue*0.15 : billValue*0.20;
-
-console.log(`The bill was ${billValue}, the tip was ${tip}, and the total value ${billValue + tip}`);
